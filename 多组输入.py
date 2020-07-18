626bdba756a7fccba8ae9f632cd34e95469ef141
xiafei-xupt <xiafei_xupt@163.com>
2020-07-18 16:26:47 +0800

    多组输入

---
+++ 多组输入.py
@@ -0,0 +1,24 @@
+'''
+
+
+'''
+
+while True:
+    try:
+        # pass
+        # 单行空格分隔 eg: 81 4
+        n, m = map(int, input().split())
+        print(n, m)
+
+        '''
+        2行一组输入，每行一个元素 eg:
+        1
+        2
+        '''
+        p = int(input())
+        k = int(input())
+        print(p, k)
+
+
+    except:
+        break
