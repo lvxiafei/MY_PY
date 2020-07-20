10bef23ed3519e227279685733478dbd78ad484c
xiafei-xupt <xiafei_xupt@163.com>
2020-07-20 16:08:42 +0800

    add 随机数组 随机数

---
+++ 随机数组.py
@@ -0,0 +1,18 @@
+'''
+
+
+'''
+import random
+def random_int_list(start, stop, length):
+  start, stop = (int(start), int(stop)) if start <= stop else (int(stop), int(start))
+  length = int(abs(length)) if length else 0
+  random_list = []
+  for i in range(length):
+    random_list.append(random.randint(start, stop))
+  return random_list
+
+print(random_int_list(0,100,10))
+
+# 生成随机数
+ret = random.randint(0, 10)
+print(ret)
