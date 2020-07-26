17c2e5cb8c3c20656ac0d3d775c7726620e1c07e
xiafei-xupt <xiafei_xupt@163.com>
2020-07-26 17:55:00 +0800

    add sort和sorted.py

---
+++ sort和sorted.py
@@ -0,0 +1,11 @@
+alist = [1, 4, 2, 3, 7, 6]
+alist.sort()
+print(alist)
+# alist.sort(reverse=True)
+print(alist)
+
+alist = [1, 4, 2, 3, 7, 6]
+print("sorted:")
+print(sorted(alist))
+print(alist)
+# print(sorted(alist, reverse=True))
