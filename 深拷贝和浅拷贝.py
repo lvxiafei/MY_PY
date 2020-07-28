de479b5c75a5f971938bb3b2ec8a773d10a49870
xiafei-xupt <xiafei_xupt@163.com>
2020-07-28 10:40:10 +0800

    拷贝成功，分配内存；浅拷一层，深拷多层

--- 深拷贝和浅拷贝.py
+++ 深拷贝和浅拷贝.py
@@ -9,9 +9,178 @@
 from copy import deepcopy
 d = {}
 d['names'] = ['Alfred', 'Bertrand']
+print(id(d))
 c = d.copy()
+print(id(c))
 dc = deepcopy(d)
+print(id(dc))
 d['names'].append('Clive')
+print(id(dc))
 print(d)
 print(c)
 print(dc)
+
+print("-" * 10)
+#   不可变类型的浅拷贝
+
+import copy  # 使用浅拷贝需要导入copy模块
+
+# 不可变类型有: 数字、字符串、元组
+
+a1 = 123123
+b1 = copy.copy(a1)  # 使用copy模块里的copy()函数就是浅拷贝了
+# 查看内存地址
+print(id(a1))
+print(id(b1))
+
+print("-" * 10)
+a2 = "abc"
+b2 = copy.copy(a2)
+# 查看内存地址
+print(id(a2))
+print(id(b2))
+
+print("-" * 10)
+a3 = (1, 2, ["hello", "world"])
+b3 = copy.copy(a3)
+# 查看内存地址
+print(id(a3))
+print(id(b3))
+'''
+不可变类型的浅拷贝说明:
+通过上面的执行结果可以得知，不可变类型进行浅拷贝不会给拷贝的对象开辟新的内存空间，
+而只是拷贝了这个对象的引用。
+'''
+
+
+print("可变类型的浅拷贝说明:")
+import copy # 使用浅拷贝需要导入copy模块
+
+# 可变类型有: 列表、字典、集合
+
+a1 = [1, 2]
+b1 = copy.copy(a1) # 使用copy模块里的copy()函数就是浅拷贝了
+# 查看内存地址
+print(id(a1))
+print(id(b1))
+print("-" * 10)
+a2 = {"name": "张三", "age": 20}
+b2 = copy.copy(a2)
+# 查看内存地址
+print(id(a2))
+print(id(b2))
+print("-" * 10)
+a3 = {1, 2, "王五"}
+b3 = copy.copy(a3)
+# 查看内存地址
+print(id(a3))
+print(id(b3))
+
+print("-" * 10)
+a4 = [1, 2, [4, 5]]
+# 注意：浅拷贝只会拷贝父对象，不会对子对象进行拷贝
+b4 = copy.copy(a4) # 使用copy模块里的copy()函数就是浅拷贝了
+# 查看内存地址
+print(id(a4))
+print(id(b4))
+print("-" * 10)
+# 查看内存地址
+print("查看子对象内存地址")
+print(id(a4[2]))
+print(id(b4[2]))
+
+# 修改数据
+a4[2][0] = 6
+
+# 子对象的数据会受影响
+print(a4)
+print(b4)
+'''
+可变类型的浅拷贝说明:
+通过上面的执行结果可以得知，可变类型进行浅拷贝只对可变类型的第一层对象进行拷贝，对拷贝的对象会开
+辟新的内存空间进行存储，子对象不进行拷贝。
+'''
+
+print("不可变类型的深拷贝示例代码:")
+import copy  # 使用深拷贝需要导入copy模块
+
+# 不可变类型有: 数字、字符串、元组
+
+a1 = 1
+b1 = copy.deepcopy(a1)  # 使用copy模块里的deepcopy()函数就是深拷贝了
+# 查看内存地址
+print(id(a1))
+print(id(b1))
+print("-" * 10)
+a2 = "张三"
+b2 = copy.deepcopy(a2)
+# 查看内存地址
+print(id(a2))
+print(id(b2))
+print("-" * 10)
+a3 = (1, 2)
+b3 = copy.deepcopy(a3)
+# 查看内存地址
+print(id(a3))
+print(id(b3))
+print("-" * 10)
+
+# 注意: 元组里面要是有可变类型对象，发现对象有可变类型就会该对象到最后一个可变类型的每一层对象进行拷贝
+a4 = (1, ["李四"])
+b4 = copy.deepcopy(a4)
+# 查看内存地址
+print(id(a4))
+print(id(b4))
+# 元组里面的可变类型子对象也会进行拷贝
+print(id(a4[1]))
+print(id(b4[1]))
+'''
+不可变类型的深拷贝说明:
+通过上面的执行结果可以得知：
+不可变类型进行深拷贝如果子对象没有可变类型则不会进行拷贝，而只是拷贝了这个对象的引用，
+否则会对该对象到最后一个可变类型的每一层对象就行拷贝, 对每一层拷贝的对象都会开辟新的内存空间进行存储
+'''
+
+print("可变类型的深拷贝示例代码:")
+import copy  # 使用深拷贝需要导入copy模块
+
+# 可变类型有: 列表、字典、集合
+
+a1 = [1, 2]
+b1 = copy.deepcopy(a1)  # 使用copy模块里的deepcopy()函数就是深拷贝了
+# 查看内存地址
+print(id(a1))
+print(id(b1))
+print("-" * 10)
+a2 = {"name": "张三"}
+b2 = copy.deepcopy(a2)
+# 查看内存地址
+print(id(a2))
+print(id(b2))
+print("-" * 10)
+a3 = {1, 2}
+print(type(a3))
+b3 = copy.deepcopy(a3)
+# 查看内存地址
+print(id(a3))
+print(id(b3))
+print("-" * 10)
+
+a4 = [1, 2, ["李四", "王五"]]
+b4 = copy.deepcopy(a4)  # 使用copy模块里的deepcopy()函数就是深拷贝了
+# 查看内存地址
+print(id(a4))
+print(id(b4))
+
+# 查看内存地址
+print(id(a4[2]))
+print(id(b4[2]))
+a4[2][0] = "王五"
+# 因为列表的内存地址不同，所以数据不会收到影响
+print(a4)
+print(b4)
+'''
+可变类型的深拷贝说明:
+通过上面的执行结果可以得知, 可变类型进行深拷贝会对该对象到最后一个可变类型的每一层对象就
+行拷贝, 对每一层拷贝的对象都会开辟新的内存空间进行存储。
+'''
