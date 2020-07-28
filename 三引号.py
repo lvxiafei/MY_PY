1d8c275694e8b69ba81a029cf194c76a6a875151
xiafei-xupt <xiafei_xupt@163.com>
2020-07-28 15:11:09 +0800

    三引号.py

---
+++ 三引号.py
@@ -0,0 +1,15 @@
+'''
+python中三引号可以将复杂的字符串进行复制:
+
+python三引号允许一个字符串跨多行，字符串中可以包含换行符、制表符以及其他特殊字符。
+三引号的语法是一对连续的单引号或者双引号（通常都是成对的用
+'''
+
+chat = """how are you?
+i'm fine, than you,and you? # test
+    me too!"""
+chat1 = '''hahahha
+h'''
+
+print(chat)
+print(chat1)
