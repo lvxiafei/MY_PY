aa175662e638b1877128960ed0b4bcca17dddfd3
xiafei-xupt <xiafei_xupt@163.com>
2020-07-31 10:15:41 +0800

    shuffle去重

--- 随机数组.py
+++ 随机数组.py
@@ -42,3 +42,39 @@ for i in range(10):
 # 检验结果
 print(ls)
 print(ls10)
+
+# shuffle截取     保证不会重复
+from random import shuffle
+
+sf = list(range(100))
+shuffle(sf)
+print(sf[:10])
+
+# shuffle 方法封装成函数
+def shuffele_func(start, stop, length):
+    sf = list(range(start,stop))
+    shuffle(sf)
+    return sorted(sf[:length])
+
+ret3 = shuffele_func(0,100,10)
+print(ret3)
+
+print("shuffle 方法封装成函数  并实现第二次升成的数据不与第一次重复:")
+# shuffle 方法封装成函数  并实现第二次升成的数据不与第一次重复
+def shuffele_func2(start, stop, length, ignore_list):
+    sf = list(range(start,stop))
+    # for i in ignore_list:
+    #     sf.remove(i)
+    [sf.remove(i) for i in ignore_list]  # 列表生成式的写法
+    print(sf)
+    shuffle(sf)
+    return sorted(sf[:length])
+ignore_list = [4, 22, 27, 31, 32, 38, 49, 64, 78, 86]
+ret4 = shuffele_func2(0,100,10,ignore_list)
+print(ret4)
+
+'''
+扩展：
+    闭包、装饰器实现 保存前几次调用的结果，实现去重
+    数据库持久化存储，实现去重
+'''
