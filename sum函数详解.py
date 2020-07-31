ef8b7c96d4456509b94de5c27895314fd2e5463b
xiafei-xupt <xiafei_xupt@163.com>
2020-07-31 11:23:12 +0800

    相加不一定是数值相加，列表亦可相加，默认start为int型0

--- sum函数详解.py
+++ sum函数详解.py
@@ -1,7 +1,7 @@
 '''
 
 sum()函数语法是这样的
-sum(iterable[, start])
+sum(iterable[, start]) # 注，start在起始位置相加
 
 iterable – 可迭代对象，如：列表(list)、元组(tuple)、集合(set)、字典(dictionary)。
 
@@ -10,5 +10,26 @@ start – 指定相加的参数，如果没有设置这个值，默认为0。
 start的值(如果没写start的值，则默认为0)
 
 '''
-sum = sum([1,2],5)
-print(sum)
+sum1 = sum([1,2],5)
+print(sum1)
+
+# 3.用sum对列表的求和
+c = [[1,2,3], [4,5,6], [7,8,9]]
+
+ret = sum(c,[])
+print(ret)
+print(sum(c,[0]))
+print(sum(c,[1]))
+print(sum(c,[0,1]))
+
+# sum 展开
+a = [1,2,3]
+b = [4,5,6]
+c = [7,8,9]
+print(sum(a,1)) # 此处加 [] 出错
+print([]+a+b+c)
+print([0]+a+b+c)
+print([1]+a+b+c)
+print([0,1]+a+b+c)
+
+# 相加不一定是数值相加，列表亦可相加，默认start为int型0
